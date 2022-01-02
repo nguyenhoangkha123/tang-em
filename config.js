77229b7787b9f03b9964f6209c4210e460be4f3e
nguyenhoangkha123 <96985850+nguyenhoangkha123@users.noreply.github.com>
2022-01-02 22:00:33 +0700

    upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Anh biết em thích anh bởi sự trưởng thành lịch lãm phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em mê Anh quá không nói nhiều :> ',
+    btnYes: 'Có, em thật sự thích anh nhiều lắm kha 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết em sao chọn đúng người như vậy :>',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai tài giỏi hiền lành nói chung anh nhất rồi',
+    mess: 'Anh biết mà khỏi nói nhiều :>. Love you😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100032219869329'
+}
